42aebd9d947782392a370eba6bae9174ed81a8cb
John Barton <john.developer.mail@gmal.com>
2021-09-19 14:59:28 -0700

    init

---
+++ core/src/main/java/com/jam18/LibGDXJAM18.java
@@ -0,0 +1,12 @@
+package com.jam18;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class LibGDXJAM18 extends ApplicationAdapter {
+
+    @Override
+    public void create() {
+
+    }
+}
---
+++ lwjgl3/src/main/java/com/jam18/lwjgl3/Lwjgl3Launcher.java
@@ -0,0 +1,24 @@
+package com.jam18.lwjgl3;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.jam18.LibGDXJAM18;
+
+/** Launches the desktop (LWJGL3) application. */
+public class Lwjgl3Launcher {
+	public static void main(String[] args) {
+		createApplication();
+	}
+
+	private static Lwjgl3Application createApplication() {
+		return new Lwjgl3Application(new LibGDXJAM18(), getDefaultConfiguration());
+	}
+
+	private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
+		Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
+		configuration.setTitle("LibGDXJam18");
+		configuration.setWindowedMode(1280, 720);
+		configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
+		return configuration;
+	}
+}
